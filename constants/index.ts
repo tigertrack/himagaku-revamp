1f4367c340e061d6258556711c0451d849f415c5
tigertrack <riandyfadly@gmail.com>
2025-01-13 04:15:31 +0900

    feat: add jlpt levels as constant

---
+++ constants/index.ts
@@ -0,0 +1,13 @@
+type jlptLevel = {
+    level: string;
+    title: string;
+    count: number;
+}
+
+export const jlptLevels: jlptLevel[] = [
+    { level: "1", title: "Mastery", count: 1232 }, 
+    { level: "2", title: "Advance", count: 367}, 
+    { level: "3", title: "Intermediate", count: 367}, 
+    { level: "4", title: "Elementary", count: 367}, 
+    { level: "5", title: "Basic",  count: 367}
+]
