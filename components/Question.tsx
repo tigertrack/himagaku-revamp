352efdb632ca839ffdb8d69f45e0fe7a7e5d65bd
tigertrack <riandyfadly@gmail.com>
2025-01-16 00:53:16 +0900

    fix: fix layouting on iphone

--- components/Question.tsx
+++ components/Question.tsx
@@ -7,52 +7,63 @@ interface Props {
   onShowChoice: () => void;
   onAnswer: (correct: string, actual: string) => void;
 }
-const Question: React.FC<Props> = ({ question, onNextQuestion, onAnswer, onShowChoice, isTimeUp }) => {
+const Question: React.FC<Props> = ({
+  question,
+  onNextQuestion,
+  onAnswer,
+  onShowChoice,
+  isTimeUp,
+}) => {
   const [showOptions, setshowOptions] = useState(false);
-  const [answer, setAnswer] = useState('');
+  const [answer, setAnswer] = useState("");
   const handleAnswer = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (!isTimeUp && !answer && question && showOptions && event.currentTarget.textContent) {
-        onAnswer(question.character, event.currentTarget.textContent)
-        setAnswer(event.currentTarget.textContent);
+    if (
+      !isTimeUp &&
+      !answer &&
+      question &&
+      showOptions &&
+      event.currentTarget.textContent
+    ) {
+      onAnswer(question.character, event.currentTarget.textContent);
+      setAnswer(event.currentTarget.textContent);
     }
   };
 
   const handleContinue = () => {
-    setAnswer('')
-    setshowOptions(false)
-    onNextQuestion()
-  }
+    setAnswer("");
+    setshowOptions(false);
+    onNextQuestion();
+  };
 
   const showChoice = () => {
-    onShowChoice()
-    setshowOptions(true)
-  }
+    onShowChoice();
+    setshowOptions(true);
+  };
 
   const BG_BORDER_CORRECT_ANSWER = "bg-teal-600 hover:border-teal-600";
   const BG_BORDER_INCORRECT_ANSWER = "bg-[#f56b6b] hover:bg-border-[#f56b6b]";
 
   const getButtonBgStyle = (textValue: string) => {
-    const isSelected = answer == textValue
-    const isRightOption = textValue == question?.character
-    const isRightAnswer = answer == question?.character
-    if(isTimeUp && isRightOption){
-      return BG_BORDER_CORRECT_ANSWER
+    const isSelected = answer == textValue;
+    const isRightOption = textValue == question?.character;
+    const isRightAnswer = answer == question?.character;
+    if (isTimeUp && isRightOption) {
+      return BG_BORDER_CORRECT_ANSWER;
+    } else if ((isSelected && isRightAnswer) || (answer && isRightOption)) {
+      return BG_BORDER_CORRECT_ANSWER;
+    } else if (isSelected && answer != question?.character) {
+      return BG_BORDER_INCORRECT_ANSWER;
+    }else if (isTimeUp || answer) {
+      return 'cursor-not-allowed'
+    } else {
+      return `hover:bg-gray-700 hover:border-bray-700`;
     }
-    else if(isSelected && isRightAnswer || answer && isRightOption){
-        return BG_BORDER_CORRECT_ANSWER
-    }
-    else if(isSelected && answer != question?.character){
-        return BG_BORDER_INCORRECT_ANSWER
-    }
-    else{
-        return `hover:bg-gray-700 hover:border-bray-700`
-    }
-  }
+  };
 
   return (
     <>
-      <div className="flex-grow flex flex-col gap-5 justify-center items-center relative px-4">
-        <div className="flex gap-3 flex-wrap landscape:flex-nowrap landscape:lg:flex-wrap landscape:lg:overflow-x-auto  landscape:overflow-x-scroll landscape:w-[80%] justify-center">
+      <div className="flex flex-col gap-5 mt-12 items-center px-4">
+        <div className="flex gap-3 flex-wrap landscape:flex-nowrap landscape:lg:flex-wrap landscape:lg:overflow-x-auto landscape:overflow-x-scroll landscape:w-[80%] justify-center">
           {question?.readings_kun.map((item, index) => (
             <span
               key={index}
@@ -70,7 +81,7 @@ const Question: React.FC<Props> = ({ question, onNextQuestion, onAnswer, onShowC
             </span>
           ))}
         </div>
-        <div className="flex gap-2 flex-wrap landscape:flex-nowrap landscape:lg:flex-wrap landscape:lg:overflow-x-auto  landscape:overflow-x-scroll landscape:w-[80%] justify-center">
+        <div className="flex gap-2 flex-wrap landscape:flex-nowrap landscape:lg:flex-wrap landscape:lg:overflow-x-auto landscape:overflow-x-scroll landscape:w-[80%] justify-center">
           {question?.meanings.map((item, index) => (
             <span
               key={index}
@@ -80,31 +91,29 @@ const Question: React.FC<Props> = ({ question, onNextQuestion, onAnswer, onShowC
             </span>
           ))}
         </div>
+      </div>
+
+      <div className="absolute bottom-0 inset-x-0">
         {(answer.length > 0 || isTimeUp) && (
-          <div className="px-3 py-1 absolute bottom-3 cursor-pointer" onClick={handleContinue}>
+          <div className="cursor-pointer text-center mb-5" onClick={handleContinue}>
             Tap here to continue
           </div>
         )}
-      </div>
-
-      <div className="relative">
         {!showOptions && (
-          <div
-            className="z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-pointer"
-            onClick={showChoice}
-          >
+          <div className="cursor-pointer text-center mb-3" onClick={showChoice}>
             show options
           </div>
         )}
-
         <div
           className={`${
             !showOptions && "opacity-0"
-          } grid landscape:grid-cols-8 grid-cols-4 landscape:lg:grid-cols-4 landscape:grid-rows-1 grid-rows-2 gap-3 self-end p-4`}
+          } grid landscape:grid-cols-8 grid-cols-4 landscape:lg:grid-cols-4 landscape:grid-rows-1 grid-rows-2 gap-3 self-end p-4  `}
         >
           {question?.choices.map((item, index) => (
             <div
-              className={`text-2xl lg:text-5xl border border-gray-600 text-center p-5 rounded ${showOptions && "cursor-pointer"} ${getButtonBgStyle(item)}`}
+              className={`text-2xl lg:text-5xl border border-gray-600 text-center p-5 rounded ${
+                showOptions && "cursor-pointer"
+              } ${getButtonBgStyle(item)}`}
               key={index}
               onClick={handleAnswer}
             >
